d03e0a3db3097f933360118f0b4648f71bc35a46
Nathaniel Collier <drxs3v3n@gmail.com>
2021-11-23 18:52:12 -0600

    Trying some stuff to get rid of browser cache

--- index.ts
+++ index.ts
@@ -7,8 +7,13 @@ app.get('/', async (req, res) => {
   res.send(await generateTableOfContents());
 });
 
-// serve the pages
-app.use('/pages', express.static('pages'));
+/*
+serve pages
+ + For info on setting etag to false see this: https://stackoverflow.com/questions/32154656/does-express-static-cache-files-in-the-memory
+*/
+app.use('/pages', express.static('pages', {
+  etag: false // prevent the browser cacheing because of HTTP 304("not modified")
+}));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
