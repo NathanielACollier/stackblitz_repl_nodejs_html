ea50545cc014d8dff86a0e4d05e6a55d66af4216
Nathaniel Collier <drxs3v3n@gmail.com>
2021-11-23 18:19:35 -0600

    Started out a way to start generating a table of contents

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
 import express from 'express';
+import {generateTableOfContents} from './server/TOC_Generator';
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send(generateTableOfContents());
 });
 
 app.listen(port, () => {
---
+++ server/TOC_Generator.ts
@@ -0,0 +1,6 @@
+
+
+export function generateTableOfContents(): string{
+
+    return "<div style='color:red;'>Hello World! - Apple Sauce</div>";
+}
